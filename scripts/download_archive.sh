178e84c64fec867cb4b3e74feb5c994db4efd001
marcinkawka <kawka.marcin@gmail.com>
2026-03-25 20:53:54 +0100

    initial commit

---
+++ scripts/download_archive.sh
@@ -0,0 +1,5 @@
+wget --mirror \
+     --no-parent \
+     --reject "index.html*" \
+     -P ./imgw_dobowe \
+     https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_hydrologiczne/dobowe/
